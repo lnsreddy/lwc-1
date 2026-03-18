df3bd3f8b6ee21642d6ef8d0a0b21642949e8904
lnsreddy <30402543+lnsreddy@users.noreply.github.com>
2026-03-18 17:25:43 +0530

    updated2

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import { LightningElement } from "lwc";
 
 export default class App extends LightningElement {
-  title = "Welcome to lwc! Lakshmi Narasimha Reddy 111";
+  title = "Welcome to lwc! Lakshmi Narasimha Reddy 1112";
 
   showFeatures = true;
 
